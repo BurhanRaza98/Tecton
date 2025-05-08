46fe25898a0fdb3472923c56a7e491ad3a1d7176
Burhan Raza <burhanraza98@gmail.com>
2025-05-08 16:12:00 +0200

    Initial Commit

---
+++ TectonTests/TectonTests.swift
@@ -0,0 +1,17 @@
+//
+//  TectonTests.swift
+//  TectonTests
+//
+//  Created by Burhan Raza on 08/05/25.
+//
+
+import Testing
+@testable import Tecton
+
+struct TectonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
